3e7853e042afb3dc1d68323d40e6c99a9c4fa02d
asehmb <a.sehmbi@outlook.com>
2026-02-20 15:49:24 -0700

    login working

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,22 @@
+import { useAuth } from '@clerk/clerk-expo'
+import { Stack } from 'expo-router'
+
+export default function AppLayout() {
+    // useAuth hook from Clerk SDK
+    const { isSignedIn } = useAuth()
+
+    return (
+        <Stack screenOptions={{ headerShown: false }}>
+            {/* Public routes */}
+            <Stack.Protected guard={!isSignedIn}>
+                <Stack.Screen name="index" />
+                <Stack.Screen name="sign-up" />
+            </Stack.Protected>
+
+            {/* Protected routes */}
+            <Stack.Protected guard={isSignedIn!}>
+                <Stack.Screen name="protected" />
+            </Stack.Protected>
+        </Stack>
+    )
+}
